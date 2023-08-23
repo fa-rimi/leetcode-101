9fdf1a296c43c2bcfd3494cb234819d0c7967f84
rimi <a.fatimaarimi@gmail.com>
2023-08-22 23:43:38 -0400

    added 412.FizzBuzz

---
+++ 412.fizzbuzz/fizzbuzzS1.js
@@ -0,0 +1,23 @@
+/**
+ * Given an integer n, return a string array answer (1-indexed) where:
+ *
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ *
+ * input: n = 15
+ * output: [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz"....."FizzBuzz"]
+ *
+ *
+ * * How would I approach this?
+ *
+ * * Firstly I know we're working with an array that will hold our string so i will
+ * * initialize an empty array to hold everything
+ *
+ * * n will be the end integer and we're iterating[i] over all the numbers between 1 and n
+ *
+ * *
+ *
+ * *
+ * */
---
+++ hash.md
@@ -0,0 +1,8 @@
+Hash -> unique ID
+Hash Function -> takes a input and returns an output
+HashSet -> stores the Hash(unique ID)
+
+set vs array:
+in order to find a value in an array you need to go through the all the items in order to find it
+but in a set you dont have to go through all of the items
+- set will use Hash Function to locate the item using its unique ID
