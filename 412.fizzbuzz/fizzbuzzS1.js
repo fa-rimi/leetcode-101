a60f79e311737940fbe4bf10e653cff3fc85d739
rimi <a.fatimaarimi@gmail.com>
2023-08-22 23:52:10 -0400

    pseudocode and breakdown of FizzBuzz

--- 412.fizzbuzz/fizzbuzzS1.js
+++ 412.fizzbuzz/fizzbuzzS1.js
@@ -10,14 +10,29 @@
  * output: [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz"....."FizzBuzz"]
  *
  *
- * * How would I approach this?
+ * ? How would I approach this?
  *
  * * Firstly I know we're working with an array that will hold our string so i will
  * * initialize an empty array to hold everything
  *
  * * n will be the end integer and we're iterating[i] over all the numbers between 1 and n
  *
- * *
+ * * We're going to be using an else...if statement for the conditions
+ * 
+ * * Everything needs to return as a string -> i(int) will need to be converted into a string 
  *
- * *
  * */
+
+/**
+ * todo: let answer be an empty array
+ * 
+ * todo: for when i=1 and i <= n, keep incrementing
+ * todo: if i is divisible by 3 && if i is divisible by 5 -> add "FizzBuzz" into answer[]
+ * todo: else if i is only divisible by 3 -> add "Fizz" into answer[]
+ * todo: else if i is only divisible by 5 -> add "Buzz" into answer[]
+ * todo: else none of the conditions are met, just add i as a string value into answer[]
+ * 
+ * todo: return the full answer array
+ * 
+ */
+
